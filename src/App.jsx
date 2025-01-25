186b8e5d2128ad567e3c2db837f2ac3e0a9a3be5
alisam132 <ali.habib132@gmail.com>
2025-01-25 22:11:43 +0300

    solved the ex.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,62 @@
-// src/App.jsx
+
+import WeatherForecast from "./WeatherForecast.jsx";
+import './App.css';
+
+const weatherForeCasts = [
+  {
+    day: "Mon",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/day.svg",
+    imgAlt: "sun icon",
+    conditions: "sunny",
+    time: "Morning",
+  },
+  {
+    day: "Tue",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/night.svg",
+    imgAlt: "moon icon",
+    conditions: "clear",
+    time: "Night",
+  },
+  {
+    day: "Wed",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/stormy.svg",
+    imgAlt: "clouds with lightning icon",
+    conditions: "stormy",
+    time: "All Day",
+  },
+  {
+    day: "Thu",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-day.svg",
+    imgAlt: "sun overcast by clouds icon",
+    conditions: "overcast",
+    time: "Evening",
+  },
+  {
+    day: "Fri",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-night.svg",
+    imgAlt: "moon overcast by clouds icon",
+    conditions: "cloudy",
+    time: "Night",
+  },
+];
 
 const App = () => {
-
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Local Weather</h1>
+      <section>
+        {weatherForeCasts.map((weatherForeCast) => (
+          <WeatherForecast
+            day={weatherForeCast.day}
+            img={weatherForeCast.img}
+            imgAlt={weatherForeCast.imgAlt}
+            conditions={weatherForeCast.conditions}
+            time={weatherForeCast.time}
+          />
+        ))}
+      </section>
+    </>
   );
-}
+};
 
-export default App
+export default App;
